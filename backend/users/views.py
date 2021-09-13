7eca58cfd8271b4e2866eb842fc13e7e100d3d98
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-09-13 11:59:44 -0700

    Forgot to save get/post functions in user file.

--- backend/users/views.py
+++ backend/users/views.py
@@ -5,3 +5,17 @@ from rest_framework.response import Response
 from rest_framework import status
 
 # Create your views here.
+
+class UserFunctions(APIView):
+    
+    def get(self, request):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

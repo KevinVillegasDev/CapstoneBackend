172915e3534f79e7999b6bb8fcd0901bce7715a6
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-09-13 08:26:04 -0700

    Made all migrations/fixed models

--- backend/recipeIngredients/models.py
+++ backend/recipeIngredients/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
+class RecipeIngredient(models.Model):
+    quantity = models.CharField(max_length=30)
+    recipeId = models.ForeignKey('recipes.Recipe', null=True, blank=True, on_delete=CASCADE)
+    ingredientId = models.ForeignKey('ingredients.Ingredient', null=True, blank=True, on_delete=CASCADE)
+    measurement = models.ForeignKey('measurements.Measurement', null=True, blank=True, on_delete=CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+    

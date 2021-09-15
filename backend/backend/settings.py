b3b6fa89ed85729506f55afbc8f0c49d400e8ac4
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-09-14 17:15:04 -0700

    Added cors to settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -147,3 +147,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
